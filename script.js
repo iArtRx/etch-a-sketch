783828665fdef83e7fc72bc4ed0792f8bd2742f5
Art <iartrx@gmail.com>
2022-08-20 11:52:54 +0700

    added reset and change resolution

--- script.js
+++ script.js
@@ -7,33 +7,57 @@ const grid = document.querySelector(".grid");
 
 
 
+
 //Create Grid
-let size = 16;
-let color = "black";
+const defaultSize = 16;
+let size = defaultSize;
+let color = "red";
 
-grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+generateGrid = (size) => {
 
-for (let i = 0; i < size * size; i++) {
-    let cell = document.createElement("div");
-    cell.style.backgroundColor = "white";
-    cell.style.border = "1px solid black"
-    grid.insertAdjacentElement("beforeend", cell);
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    for (let i = 0; i < size * size; i++) {
+        let cell = document.createElement("div");
+        cell.style.backgroundColor = "white";
+        cell.style.border = "1px solid black"
+        grid.insertAdjacentElement("beforeend", cell)   
+    
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = color;
+        });
+    };
+
+    
+
+ 
+};
+
+generateGrid(defaultSize);
 
 
-    cell.addEventListener("mouseover", e => {
-        cell.style.backgroundColor = color;
-    });
 
-}
 
 const colourPicker = document.querySelector("#colour")
-
-
 colourPicker.addEventListener("click", () => {
         color = "pink";
-        console.log(pink)
     });
+
+const reset = document.querySelector("#reset")
+reset.addEventListener("click", () => {
+    grid.innerHTML="";
+    generateGrid(size);
+});
+
+const resolution = document.querySelector("#resolution");
+resolution.addEventListener("click", e => {
+    let select = prompt("Enter a resolution:");
+    size = parseInt(select);
+});
+
+
+
 
 
     
